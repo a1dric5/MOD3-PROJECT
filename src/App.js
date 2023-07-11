0f4c41fdeb2599e0a35570099f7fbba730c1e7c9
Aldrich Lapid <aldrichlapid@gmail.com>
2023-07-10 23:24:48 -0700

    added js and css files to src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 
+import About from './src/pages/about';
+// import ApplicationForm from './pages/applicationform';
+import Contact from './src/pages/contact';
+import Navbar from './components/navbar';
+// import SummerCamp from './components/SummerCamp';
+// import CareerAdvice from './components/CareerAdvice';
+
 function App() {
+  // const [open, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router basename="/">
+
+        {/* Add Menu Component */}
+        <Navbar setOpen={setOpen} />
+        
+        <Switch> 
+          <Route exact path="/" component={About}/>
+          {/* <Route path="/ApplicationForm" render={() => <ApplicationForm open={open} setOpen={setOpen} />}/>
+          <Route path="/SummerCamp" component={SummerCamp}/>
+          <Route path="/CareerAdvice" component={CareerAdvice}/> */}
+          <Route path="/Contact" component={Contact}/>
+
+          
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import './index.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <SearchRoundedIcon />
+      {/* Additional content */}
+    </div>
+  );
+};
+
+export default Navbar;
